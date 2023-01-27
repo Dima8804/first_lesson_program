ad534a72d04e19f7d51231e910a868bd1b28ed0c
Dima Mynka <mdv8804@gmail.com>
2023-01-27 11:04:39 +0200

    IF Else

---
+++ Exampl005_IFElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("put name :");
+string username = Console.ReadLine(); 
+
+if (username.ToLower == "Masha")
+{
+   Console.WriteLine("YRA thi is Masha") ;
+}
+
+else
+{
+    Console.WriteLine("Hello,");
+    Console.WriteLine(username);
+}
