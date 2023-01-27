e624208142d11cadf29cb3d04e57c18cd1532891
Dima Mynka <mdv8804@gmail.com>
2023-01-27 11:15:51 +0200

    Base

---
+++ Exampl006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 4;
+int c = 6;
+int d = 7;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c> max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.Write(max);
